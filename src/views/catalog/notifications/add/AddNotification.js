8c547ba17a6300f57ddbf18446cf0079aeea80c8
osmankoyuncu53 <osmankoyuncu396@gmail.com>
2023-06-05 20:44:25 +0300

    Feature / Notifications

--- src/views/catalog/notifications/add/AddNotification.js
+++ src/views/catalog/notifications/add/AddNotification.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-import './../Notification.css'
 
 import {
   CCard,
@@ -16,53 +15,122 @@ import {
   CFormFeedback,
   CButton,
   CFormTextarea,
+  CFormLabel,
 } from '@coreui/react'
+import { AddNotifications } from 'src/api/catalog/NotificationAPI'
 
 const NotificationAdd = () => {
+
+  const [type, setType] = useState(null)
+  const [title, setTitle] = useState(null)
+  const [message, setMessage] = useState(null)
+
+  const [studentId, setStudentId] = useState(null)
+  const [courseId, setCourseId] = useState(null)
+
+  const [selectedValueType, setSelectedValueType] = useState(null)
+
   const [validated, setValidated] = useState(false)
+
+
+  const body = {
+    type: parseInt(type),
+    title: title,
+    message: message,
+  }
+
+  if(studentId){
+    body.studentId = parseInt(studentId)
+  }
+  if(courseId){
+    body.courseId = parseInt(courseId)
+  }
+
   const handleSubmit = (event) => {
     const form = event.currentTarget
     if (form.checkValidity() === false) {
-      event.preventDefault()
       event.stopPropagation()
+      setValidated(true)
     }
-    setValidated(true)
+    else {
+      setValidated(false)
+      AddNotifications(body)  // Ekleme fonksiyonu
+    }
+    event.preventDefault()
   }
+
+
   return (
     <CContainer>
       <CCard>
         <CCardHeader className="bg-dark">
-          <strong>Bildirim Ekle</strong>
+          <span className='text-white'>Bildirim Ekle</span>
         </CCardHeader>
         <CCardBody>
           <CForm
-            className="row needs-validation"
+            className="row needs-validation my-form"
+            id='my-form'
             noValidate
             validated={validated}
             onSubmit={handleSubmit}
           >
             <CRow>
               <CCol sm="6">
-                <CFormSelect label="Bildirim Tipi">
-                  <option>Seçiniz</option>
-                  <option>Admin</option>
-                  <option>Kullanıcı</option>
+                <CFormSelect onChange={e => setType(e.target.value)} name='type' label="Tip:" required>
+                  <option value={""}>Seçiniz</option>
+                  <option value={0}>SMS</option>
+                  <option value={1}>Mail</option>
+                  <option value={2}>Bildirim</option>
                 </CFormSelect>
                 <CFormFeedback invalid>Lütfen bildirim tipi giriniz.</CFormFeedback>
               </CCol>
 
               <CCol sm="6">
-                <CFormSelect label="Kime">
-                  <option>Seçiniz</option>
-                  <option>Admin</option>
-                  <option>Kullanıcı</option>
+                <CFormSelect onChange={e => setSelectedValueType(e.target.value)} name='select_person' label="Kime" required>
+                  <option value={""}>Seçiniz</option>
+                  <option value={1}>Tüm öğrenciler</option>
+                  <option value={2}>Seçilen sınıfa</option>
+                  <option value={3}>Seçilen öğrencilere</option>
                 </CFormSelect>
                 <CFormFeedback invalid>Lütfen kime gönderileceğini seçiniz.</CFormFeedback>
               </CCol>
             </CRow>
 
             <CRow className="mt-4">
+              <CCol sm="6">
+                <CFormInput onChange={e => setTitle(e.target.value)} name='title' type="text" label="Başlık" required />
+                <CFormFeedback invalid>Lütfen başlık giriniz.</CFormFeedback>
+              </CCol>
+
+              {selectedValueType == 2 && (
+                <CCol sm="6">
+                  <CFormSelect label="Sınıf Seçiniz" onChange={e => setCourseId(e.target.value)} required>
+                    <option value={""}>Seçiniz</option>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                  </CFormSelect>
+                  <CFormFeedback invalid>Lütfen sınıf seçiniz.</CFormFeedback>
+                </CCol>
+              )}
+
+              {selectedValueType == 3 && (
+                <CCol sm="6">
+                  <CFormSelect label="Öğrenci Seçiniz" onChange={e => setStudentId(e.target.value)} required>
+                    <option value={""}>Seçiniz</option>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={10}>10</option>
+                  </CFormSelect>
+                  <CFormFeedback invalid>Lütfen öğrenci seçiniz.</CFormFeedback>
+                </CCol>
+              )}
+
+            </CRow>
+
+            <CRow className="mt-4">
               <CCol sm="12">
+                <CFormLabel>Açıklama</CFormLabel>
                 <CKEditor
                   editor={ClassicEditor}
                   data="Lütfen mesajınızı giriniz."
@@ -71,21 +139,17 @@ const NotificationAdd = () => {
                   }}
                   onChange={(event, editor) => {
                     const data = editor.getData()
-                    console.log({ event, editor, data })
+                    setMessage(data)
                   }}
-                  onBlur={(event, editor) => {
-                    console.log('Blur.', editor)
-                  }}
-                  onFocus={(event, editor) => {
-                    console.log('Focus.', editor)
-                  }}
+                  required
                 />
+                <CFormFeedback invalid>Lütfen açıklama giriniz.</CFormFeedback>
               </CCol>
             </CRow>
 
             <CRow className="mt-4">
               <CCol sm="12">
-                <CButton color="primary" type="submit" className="float-end mt-3">
+                <CButton color="primary" type="submit" className="float-end mt-3" style={{ width: '100%' }}>
                   Kaydet
                 </CButton>
               </CCol>
--- src/views/catalog/notifications/list/Notifications.js
+++ src/views/catalog/notifications/list/Notifications.js
@@ -5,72 +5,97 @@ import {
   CCardHeader,
   CCol,
   CContainer,
+  CFormInput,
   CFormLabel,
   CRow,
 } from '@coreui/react'
-import React, { useCallback, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDataGrid from '@inovua/reactdatagrid-community'
 import '@inovua/reactdatagrid-community/index.css'
-import {
-  columns,
-  dataSource,
-  defaultFilterValue,
-} from 'src/dami_data/notifications/NotificationData'
-import CIcon from '@coreui/icons-react'
-import { cilBell } from '@coreui/icons'
+import { cilPlus } from '@coreui/icons'
+import { GridLinkDelete, GridLinkUpdate, gridStyle } from 'src/definitions/GridLink'
+import { IconDatatableHead, SpanDatatableHead } from 'src/definitions/DatatableHeader'
+import { downloadExcel } from "react-export-table-to-excel"
+import { FaFileExcel } from "react-icons/fa";
+import { BASE_URL } from 'src/config/Config'
+import { GetAllNotifications } from 'src/api/catalog/NotificationAPI'
 
-const gridStyle = { minHeight: 550, marginTop: 10 }
+const defaultFilterValue = [
+  { name: 'name', operator: 'startsWith', type: 'string' },
+  { name: 'email', operator: 'startsWith', type: 'string' },
+]
 
-const loadData = ({ skip, limit, sortInfo }) => {
-  const url =
-    dataSource + '?skip=' + skip + '&limit=' + limit + '&sortInfo=' + JSON.stringify(sortInfo)
-
-  return fetch(url).then((response) => {
-    const totalCount = response.headers.get('X-Total-Count')
-    return response.json().then((data) => {
-      return Promise.resolve({ data, count: parseInt(totalCount) })
-    })
-  })
-}
+const title = [
+  { name: 'id', type: 'number', maxWidth: 40, header: 'ID', defaultVisible: false },
+  { name: 'name', defaultFlex: 2, header: 'Ad' },
+  { name: 'email', defaultFlex: 3, header: 'Email' },
+  { name: 'password', defaultFlex: 3, header: 'Şifre' },
+  { name: 'role', defaultFlex: 3, header: 'Role' },
+  { name: 'status', defaultFlex: 3, header: 'Statü' },
+]
 
 const Notifications = () => {
-  //const dataSource = useCallback(loadData, []);
+  
+  // Export Excel
+  const exportHeader = ["id", "name", "email", "role", "status"];
 
-  const renderRowContextMenu = (menuProps, { rowProps }) => {
-    menuProps.autoDismiss = true
-    menuProps.items = [
-      {
-        label: 'Row ' + rowProps.rowIndex,
+  function HandleDownloadExcel() {
+    downloadExcel({
+      fileName: "Bildirimler",
+      sheet: "react-export-table-to-excel",
+      tablePayload: {
+        header: exportHeader,
+        body: notifications,
       },
-    ]
+    });
   }
+
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    GetAllNotifications()
+      .then((response) => {
+        setNotifications(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []);
+
   return (
     <CContainer>
       <CRow>
         <CCol>
           <CCard>
-            <CCardHeader className="bg-dark">
+          <CCardHeader className="bg-dark">
               <CFormLabel className="mt-1 text-light">Bildirimler</CFormLabel>
+
               <CButton
                 className="float-end bg-light text-dark"
-                href={process.env.REACT_APP_BASE_URL + 'catalog/notifications/add'}
+                href={BASE_URL + 'catalog/notifications/add'}
               >
-                <CIcon icon={cilBell} className="mx-2" />
-                Bildirim Ekle
+                <IconDatatableHead icon={cilPlus}></IconDatatableHead>
+                <SpanDatatableHead text={'Bildirim Ekle'}></SpanDatatableHead>
               </CButton>
             </CCardHeader>
 
             <CCardBody>
-              <ReactDataGrid
-                idProperty="id"
-                defaultFilterValue={defaultFilterValue}
-                renderRowContextMenu={renderRowContextMenu}
-                style={gridStyle}
-                columns={columns}
-                pagination
-                dataSource={dataSource}
-                defaultLimit={10}
-              />
+            <CButton className="float-middle bg-light text-dark" onClick={HandleDownloadExcel}>
+                <FaFileExcel></FaFileExcel>
+                <span>Export Excel</span>
+              </CButton>
+
+              {notifications.length > 0 && (
+                <ReactDataGrid
+                  idProperty="id"
+                  style={gridStyle}
+                  columns={title}
+                  pagination
+                  dataSource={notifications}
+                  defaultLimit={10}
+                  defaultFilterValue={defaultFilterValue}
+                />
+              )}
             </CCardBody>
           </CCard>
         </CCol>
