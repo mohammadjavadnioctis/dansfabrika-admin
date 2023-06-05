b3925d1688ac9d27226881680579bfcda2f31e45
osmankoyuncu53 <osmankoyuncu396@gmail.com>
2023-06-05 20:43:05 +0300

    Feature / Bills

---
+++ src/api/catalog/BillsAPI.js
@@ -0,0 +1,92 @@
+import ApiManager from "../ApiManager";
+import Swal from 'sweetalert2'
+import { GetBasicSwal, GetBasicSwalUrl, GetSwal } from "src/definitions/Alert";
+
+
+
+export async function GetAllBills() {
+    try {
+        const data = await ApiManager('bills/', {
+            method: 'GET',
+        })
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function DeleteBill(id) {
+    try {
+        Swal.fire({
+            title: 'Silmek istediğinize emin misiniz?',
+            showCancelButton: true,
+            confirmButtonText: 'Evet, eminim',
+            cancelButtonText: 'İptal Et'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const data = ApiManager('bills/' + id, {
+                    method: 'DELETE',
+                })
+                    .then((response) => {
+                        if (response.status = 200) {
+                            GetBasicSwal('Başarılı!', 'İşlem başarıyla silindi', 'success')
+                        }
+                    })
+                    .catch((error) => {
+                        Swal.fire('Başarısız!', error.response.data.message[0], 'error')
+                    })
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function AddBill(body) {
+    try {
+        const data = ApiManager('bills', {
+            method: 'POST',
+            data: body,
+        })
+            .then((response) => {
+                if (response.status = 200) {
+                    GetBasicSwalUrl('Başarılı!', 'Gelir veya gider başarıyla eklendi', 'success', 'catalog/bills/list')
+                }
+            })
+            .catch((error) => {
+                GetSwal('Başarısız!', error.response.data.message[0], 'error')
+            })
+    } catch (error) {
+        GetSwal('Hata', error.response.data['message'], 'error')
+    }
+}
+
+export async function GetByIdBill(id) {
+    try {
+        const data = await ApiManager('bills/'+id, {
+            method: 'GET',
+        })
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function UpdateBill(body) {
+    try {
+        const data = ApiManager('bills', {
+            method: 'PUT',
+            data: body,
+        })
+            .then((response) => {
+                if (response.status = 200) {
+                    GetBasicSwalUrl('Başarılı!', 'Gelir veya gider başarıyla güncellendi', 'success', 'catalog/bills/list')
+                }
+            })
+            .catch((error) => {
+                GetSwal('Başarısız!', error.response.data.message[0], 'error')
+            })
+    } catch (error) {
+        GetSwal('Hata', error.response.data['message'], 'error')
+    }
+}
---
+++ src/views/catalog/bills/add/AddBill.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import {
+    CCard,
+    CCardBody,
+    CCardHeader,
+    CCol,
+    CContainer,
+    CForm,
+    CFormInput,
+    CFormSelect,
+    CRow,
+    CFormFeedback,
+    CButton,
+    CFormTextarea,
+    CFormLabel,
+} from '@coreui/react'
+import { AddBill } from 'src/api/catalog/BillsAPI'
+
+const BillAdd = () => {
+
+    const [processType, setProccessType] = useState(null)
+    const [process, setProccess] = useState(null)
+    const [description, setDescription] = useState(null)
+    const [processDate, setProccessDate] = useState(null)
+    const [price, setPrice] = useState(null)
+
+    const [validated, setValidated] = useState(false)
+
+    const body = {
+        processType: parseInt(processType),
+        process: parseInt(process),
+        description: description,
+        processDate: processDate,
+        price: parseFloat(price)
+    }
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget
+        if (form.checkValidity() === false) {
+            event.stopPropagation()
+            setValidated(true)
+        }
+        else {
+            setValidated(false)
+            AddBill(body)  // Ekleme fonksiyonu
+        }
+        event.preventDefault()
+    }
+
+
+
+    return (
+        <CContainer>
+            <CCard>
+                <CCardHeader className="bg-dark">
+                    <span className='text-white'>Gelir veya Gider Ekle</span>
+                </CCardHeader>
+                <CCardBody>
+                    <CForm
+                        className="row needs-validation my-form"
+                        id='my-form'
+                        noValidate
+                        validated={validated}
+                        onSubmit={handleSubmit}
+                    >
+                        <CRow>
+                            <CCol sm="6">
+                                <CFormSelect onChange={e => setProccessType(e.target.value)} name='proccess_type' label="İşlem Tipi:" required>
+                                    <option value={0}>Seçiniz</option>
+                                    <option value={1}>Admin</option>
+                                    <option value={2}>Kullanıcı</option>
+                                </CFormSelect>
+                                <CFormFeedback invalid>Lütfen işlem tipi seçiniz.</CFormFeedback>
+                            </CCol>
+
+                            <CCol sm="6">
+                                <CFormSelect onChange={e => setProccess(e.target.value)} name='proccess' label="İşlem:" required>
+                                    <option value={0}>Seçiniz</option>
+                                    <option value={1}>Admin</option>
+                                    <option value={2}>Kullanıcı</option>
+                                </CFormSelect>
+                                <CFormFeedback invalid>Lütfen işlem seçiniz.</CFormFeedback>
+                            </CCol>
+                        </CRow>
+
+                        <CRow className='mt-4'>
+                            <CCol sm="6">
+                                <CFormInput onChange={e => setProccessDate(e.target.value)} type='date' name='proccess_date' label='İşlem Tarihi' required></CFormInput>
+                                <CFormFeedback invalid>Lütfen Tarih seçiniz.</CFormFeedback>
+                            </CCol>
+
+                            <CCol sm="6">
+                                <CFormInput onChange={e => setPrice(e.target.value)} type='number' name='price' label='Fiyat' required></CFormInput>
+                                <CFormFeedback invalid>Lütfen rol seçiniz.</CFormFeedback>
+                            </CCol>
+                        </CRow>
+
+                        <CRow className='mt-4'>
+                            <CCol sm="12">
+                                <CFormTextarea onChange={e => setDescription(e.target.value)} name='description' label='Açıklama' rows='5' required></CFormTextarea>
+                                <CFormFeedback invalid>Lütfen Açıklama Giriniz.</CFormFeedback>
+                            </CCol>
+                        </CRow>
+
+                        <CRow className="mt-4">
+                            <CCol sm="12">
+                                <CButton color="primary" type="submit" className="float-end mt-3" style={{ width: '100%' }}>
+                                    Kaydet
+                                </CButton>
+                            </CCol>
+                        </CRow>
+                    </CForm>
+                </CCardBody>
+            </CCard>
+        </CContainer>
+    )
+}
+
+export default BillAdd
