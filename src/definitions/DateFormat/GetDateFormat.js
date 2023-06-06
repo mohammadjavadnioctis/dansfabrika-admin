cb48c85c772770a0f333efcb2a27fa971e604a13
osmankoyuncu53 <osmankoyuncu396@gmail.com>
2023-06-06 15:06:59 +0300

    Update GetDateFormat.js

--- src/definitions/DateFormat/GetDateFormat.js
+++ src/definitions/DateFormat/GetDateFormat.js
@@ -3,6 +3,13 @@ import { format } from 'date-fns'
 
 export function SetDateFormat(date) {
     const isoDate = date;
+    const formattedDate = format(new Date(isoDate), 'yyyy-MM-dd');
+
+    return formattedDate;
+}
+
+export function SetDateTimeFormat(date) {
+    const isoDate = date;
     const formattedDate = format(new Date(isoDate), 'yyyy-MM-dd HH:mm');
 
     return formattedDate;
