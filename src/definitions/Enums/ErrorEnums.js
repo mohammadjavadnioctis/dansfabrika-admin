a7cfa6109955b7004e37110f6eafce5997cdf475
osmankoyuncu53 <osmankoyuncu396@gmail.com>
2023-07-05 09:40:00 +0300

    update error enum

--- src/definitions/Enums/ErrorEnums.js
+++ src/definitions/Enums/ErrorEnums.js
@@ -1,92 +1,96 @@
 import React from 'react'
 
 const ErrorEnum = {
-    AUTH_EMAIL_PASSWORD_ERROR: 'AUTH_EMAIL_PASSWORD_ERROR',
-    AUTH_2FA_ERROR: 'AUTH_2FA_ERROR',
+    AUTH_EMAIL_PASSWORD_ERROR: 'Eposta veya parola hatalı',
+    AUTH_2FA_ERROR: 'Onay kodu hatalı',
 
-    QUEUE_REQUIRED: 'QUEUE_REQUIRED',
-    INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
+    QUEUE_REQUIRED: 'Sıra boş geçilemez',
+    INTERNAL_SERVER_ERROR: 'Bilinmeyen bir hata ile karşılaşıldı. Daha sonra tekrar deneyiniz',
 
-    EMAIL_REQUIRED: 'EMAIL_REQUIRED',
-    EMAIL_UNIQUE: 'EMAIL_UNIQUE',
-    EMAIL_INVALID: 'EMAIL_INVALID',
-    SMS_POST_ERROR: 'SMS_POST_ERROR',
-    CODE_REQUIRED: 'CODE_REQUIRED',
+    EMAIL_REQUIRED: 'Eposta boş geçilemez',
+    EMAIL_UNIQUE: 'Bu eposta zaten kullanılıyor',
+    EMAIL_INVALID: 'Geçersiz eposta',
+    SMS_POST_ERROR: 'SMS gönderim hatası',
+    CODE_REQUIRED: 'Onay kodu boş geçilemez',
 
-    TITLE_REQUIRED: 'TITLE_REQUIRED',
-    MESSAGE_REQUIRED: 'MESSAGE_REQUIRED',
+    TITLE_REQUIRED: 'Başlık boş geçilemez',
+    MESSAGE_REQUIRED: 'Mesaj boş geçilemez',
 
-    PROCESS_DATE_REQUIRED: 'PROCESS_DATE_REQUIRED',
-    PROCESS_REQUIRED: 'PROCESS_REQUIRED',
-    PROCESS_TYPE_REQUIRED: 'PROCESS_TYPE_REQUIRED',
+    PROCESS_DATE_REQUIRED: 'İşlem tarihi boş geçilemez',
+    PROCESS_REQUIRED: 'İşlem boş geçilemez',
+    PROCESS_TYPE_REQUIRED: 'İşlem tipi boş geçilemez',
 
-    NEW_PASSWORD_REQUIRED: 'NEW_PASSWORD_REQUIRED',
-    OLD_PASSWORD_AGAIN_REQUIRED: 'OLD_PASSWORD_AGAIN_REQUIRED',
-    OLD_PASSWORD_REQUIRED: 'OLD_PASSWORD_REQUIRED',
-    OLD_AND_NEW_DOESNT_MATCH: 'OLD_AND_NEW_DOESNT_MATCH',
+    NEW_PASSWORD_REQUIRED: 'Yeni parola boş geçilemez',
+    OLD_PASSWORD_AGAIN_REQUIRED: 'Eski parola boş geçilemez',
+    OLD_PASSWORD_REQUIRED: 'Eski parola boş geçilemez',
+    OLD_AND_NEW_DOESNT_MATCH: 'Parolalar eşleşmiyor',
 
-    COURSE_REQUIRED: 'COURSE_REQUIRED',
-    ATTENDANCE_DATE_REQUIRED: 'ATTENDANCE_DATE_REQUIRED',
+    COURSE_REQUIRED: 'Akademi boş geçilemez',
+    ATTENDANCE_DATE_REQUIRED: 'Yoklama tarihi boş geçilemez',
 
-    DAY_REQUIRED: 'DAY_REQUIRED',
-    LESSON_REQUIRED: 'LESSON_REQUIRED',
+    DAY_REQUIRED: 'Gün boş geçilemez',
+    LESSON_REQUIRED: 'Ders boş geçilemez',
 
-    ONSALE_REQUIRED: 'ONSALE_REQUIRED',
-    COURSETYPE_REQUIRED: 'COURSETYPE_REQUIRED',
-    TRAINER_REQUIRED: 'TRAINER_REQUIRED',
-    CAPACITY_REQUIRED: 'CAPACITY_REQUIRED',
-    DANCE_LEVEL_REQUIRED: 'DANCE_LEVEL_REQUIRED',
-    DANCE_TYPE_REQUIRED: 'DANCE_TYPE_REQUIRED',
+    ONSALE_REQUIRED: 'Satış aktifliği boş geçilemez',
+    COURSETYPE_REQUIRED: 'Akademi tipi boş geçilemez',
+    TRAINER_REQUIRED: 'Eğitmen boş geçilemez',
+    CAPACITY_REQUIRED: 'Kapasite boş geçilemez',
+    DANCE_LEVEL_REQUIRED: 'Dans seviyesi boş geçilemez',
+    DANCE_TYPE_REQUIRED: 'Dans tipi boş geçilemez',
 
-    END_DATE_REQUIRED: 'END_DATE_REQUIRED',
-    START_DATE_REQUIRED: 'START_DATE_REQUIRED',
+    END_DATE_REQUIRED: 'Bitiş tarihi boş geçilemez',
+    START_DATE_REQUIRED: 'Başlangıç tarihi boş geçilemez',
 
-    END_TIME_REQUIRED: 'END_TIME_REQUIRED',
-    START_TIME_REQUIRED: 'START_TIME_REQUIRED',
+    END_TIME_REQUIRED: 'Bitiş saati boş geçilemez',
+    START_TIME_REQUIRED: 'Başlangıç saati boş geçilemez',
 
-    PRICE_REQUIRED: 'PRICE_REQUIRED',
+    PRICE_REQUIRED: 'Fiyat boş geçilemez',
 
-    IDENTITY_REQUIRED: 'IDENTITY_REQUIRED',
+    IDENTITY_REQUIRED: 'Kimlik no boş geçilemez',
 
-    CREDIT_REQUIRED: 'CREDIT_REQUIRED',
+    CREDIT_REQUIRED: 'Katılım hakkı boş geçilemez',
 
-    SELLBY_REQUIRED: 'SELLBY_REQUIRED',
+    SELLBY_REQUIRED: 'Satış tarihi boş geçilemez',
 
-    ID_REQUIRED: 'ID_REQUIRED',
+    ID_REQUIRED: 'ID boş geçilemez',
 
-    STUDENT_REQUIRED: 'STUDENT_REQUIRED',
+    STUDENT_REQUIRED: 'Öğrenci boş geçilemez',
 
-    IMAGE_REQUIRED: 'IMAGE_REQUIRED',
-    DESCRIPTION_REQUIRED: 'DESCRIPTION_REQUIRED',
+    IMAGE_REQUIRED: 'Görsel boş geçilemez',
+    DESCRIPTION_REQUIRED: 'Açıklama boş geçilemez',
 
-    ID_MUST_NUMBER: 'ID_MUST_NUMBER',
+    ID_MUST_NUMBER: 'ID sayısal veri tipinde olmalıdır',
 
-    PASSWORD_REQUIRED: 'ŞİFRE ZORUNLU ALAN',
-    PASSWORD_MIN: 'PASSWORD_MIN',
+    PASSWORD_REQUIRED: 'Parola boş geçilemez',
+    PASSWORD_MIN: 'Parolanız minimum 8 karakter olmalıdır',
 
-    NAME_LENGTH: 'NAME_LENGTH',
+    NAME_LENGTH: 'İsim boş geçilemez',
 
-    TIME_REQUIRED: 'TIME_REQUIRED',
+    TIME_REQUIRED: 'Tarih/Zaman boş geçilemez',
 
-    TYPE_REQUIRED: 'TYPE_REQUIRED',
-    STUDENT_OR_COURSE_REQUIRED: 'STUDENT_OR_COURSE_REQUIRED',
+    TYPE_REQUIRED: 'Tip boş geçilemez',
+    STUDENT_OR_COURSE_REQUIRED: 'Öğrenci veya akademi boş geçilemez',
 
-    REFEREDID_REQUIRED: 'REFEREDID_REQUIRED',
+    REFEREDID_REQUIRED: 'Referans ID boş geçilemez',
 
-    NAME_REQUIRED: 'NAME_REQUIRED',
-    NAME_MAX: 'NAME_MAX',
+    NAME_REQUIRED: 'İsim boş geçilemez',
+    NAME_MAX: 'İsim maksimum 60 karakter olabilir',
 
-    STATUS_REQUIRED: 'STATUS_REQUIRED',
+    STATUS_REQUIRED: 'Durum boş geçilemez',
 
-    ROLE_REQUIRED: 'ROL ZORUNLU ALAN',
+    ROLE_REQUIRED: 'Rol/Yetki boş geçilemez',
 
-    PHONE_REQUIRED: 'PHONE_REQUIRED',
-    PHONE_UNIQUE: 'PHONE_UNIQUE',
-    PHONE_INVALID: 'PHONE_INVALID',
+    PHONE_REQUIRED: 'Telefon boş geçilemez',
+    PHONE_UNIQUE: 'Bu telefon numarası zaten sistemde kayıtlı',
+    PHONE_INVALID: 'Geçersiz telefon numarası',
 
-    HAVENT_RECORD: 'HAVENT_RECORD',
+    HAVENT_RECORD: 'Kayıt bulunamadı',
 
-    BAD_REQUEST: 'BAD_REQUEST',
+
+    NO_COURSE_RECORD: 'Bu kursa kaydınız bulunmamaktadır',
+
+    BAD_REQUEST: 'Eksik veya hatalı bilgiler',
+    PRIOTRY_REQUIRED: 'Öncelik boş geçilemez',
 }
 
 export function ControlErrorMessage(message) {
@@ -96,6 +100,6 @@ export function ControlErrorMessage(message) {
             return ErrorEnum[key];
         }
     }
-    
+
     return message;
 }
